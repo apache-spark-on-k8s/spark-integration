989a371094718156cce45b68e59a98d191af4c72
Kimoon Kim <kimoon@pepperdata.com>
2017-12-18 13:32:58 -0800

    Switch to container.image property keys

--- integration-test/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesTestComponents.scala
+++ integration-test/src/test/scala/org/apache/spark/deploy/k8s/integrationtest/KubernetesTestComponents.scala
@@ -57,11 +57,9 @@ private[spark] class KubernetesTestComponents(defaultClient: DefaultKubernetesCl
     new SparkAppConf()
       .set("spark.master", s"k8s://${kubernetesClient.getMasterUrl}")
       .set("spark.kubernetes.namespace", namespace)
-      // TODO: apache/spark#19995 is changing docker.image to container.image in these properties.
-      // Update them once the PR is merged.
-      .set("spark.kubernetes.driver.docker.image",
+      .set("spark.kubernetes.driver.container.image",
         System.getProperty("spark.docker.test.driverImage", "spark-driver:latest"))
-      .set("spark.kubernetes.executor.docker.image",
+      .set("spark.kubernetes.executor.container.image",
         System.getProperty("spark.docker.test.executorImage", "spark-executor:latest"))
       .set("spark.executor.memory", "500m")
       .set("spark.executor.cores", "1")
