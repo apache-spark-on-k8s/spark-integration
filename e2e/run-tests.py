1a745dba77d2b5180c231dda6f78a45a60e956f8
foxish <ramanathana@google.com>
2017-12-21 21:09:35 -0800

    Better than ever

--- e2e/run-tests.py
+++ e2e/run-tests.py
@@ -12,11 +12,10 @@ def clone_repo():
   # print subprocess.check_output(['./sbin/build-push-docker-images.sh', '-r', 'foxish', '-t', '0.1', 'build'])
   # print subprocess.check_output(['./sbin/build-push-docker-images.sh', '-r', 'foxish', '-t', '0.1', 'push'])
 
-  os.chdir("../../spark-integration")
-  print subprocess.check_output(['mvn', 'clean', 'integration-test',
-                                 '-Ddownload.plugin.skip=true',
-                                 '-Dspark-distro-tgz=../spark/',
-                                 '-DextraScalaTestArgs="-Dspark.kubernetes.test.master=k8s://https://35.197.21.13 -Dspark.docker.test.driverImage=foxish/spark-driver:0.1 -Dspark.docker.test.executorImage=foxish/spark-executor:0.1"'])
+  os.chdir("../../spark-integration/integration-test")
+  os.system("mvn clean -Ddownload.plugin.skip=true integration-test \
+            -Dspark-distro-tgz=/home/ramanathana/go-workspace/src/apache-spark-on-k8s/spark-integration/e2e/spark/spark-2.3.0-SNAPSHOT-bin-test-spark.tgz -Dspark-dockerfiles-dir=/home/ramanathana/go-workspace/src/apache-spark-on-k8s/spark-integration/e2e/spark/dist/kubernetes/dockerfiles/ \
+            -DextraScalaTestArgs=\"-Dspark.kubernetes.test.master=k8s://https://35.197.21.13 -Dspark.docker.test.driverImage=foxish/spark-driver:0.1 -Dspark.docker.test.executorImage=foxish/spark-executor:0.1\"")
 
 """
 mvn clean -Ddownload.plugin.skip=true integration-test  \
