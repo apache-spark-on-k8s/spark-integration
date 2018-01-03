79da20f04fcd40e3d70cc3a3de6356baf7a09d28
foxish <ramanathana@google.com>
2018-01-02 16:14:28 -0800

    added

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
 set -ex
-apt-get update && apt-get install -y curl git
+echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
+apt-get update && apt-get install -y curl wget git tar openjdk-8-jdk
+
+wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar xvf apache-maven-3.5.2-bin.tar.gz
+root=$(pwd)
+export PATH=$root/apache-maven-3.5.2:$PATH
+
 git clone https://github.com/apache/spark
+git clone https://github.com/apache-spark-on-k8s/spark-integration
 cd spark && ./dev/make-distribution.sh --tgz -Phadoop-2.7 -Pkubernetes -DskipTests
-cd dist && ./sbin/build-push-docker-images.sh -r gcr.io/spark-k8s-test -t latest build && \
-           ./sbin/build-push-docker-images.sh -r gcr.io/spark-k8s-test -t latest push
+tag=(git rev-parse HEAD | cut -c -6)
+
+cd dist && ./sbin/build-push-docker-images.sh -r gcr.io/swarm-1358 -t $tag build && \
+           gcloud docker -- push gcr.io/swarm-1358/spark-driver:$tag && \
+           gcloud docker -- push gcr.io/swarm-1358/spark-executor:$tag && \
+           gcloud docker -- push gcr.io/swarm-1358/spark-init:$tag
+
 cd ../../spark-integration/integration-test
 
 cluster=kubectl cluster-info | head -n 1 | grep -oE "https?://\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 mvn clean -Ddownload.plugin.skip=true integration-test \
-          -Dspark-distro-tgz=/home/ramanathana/go-workspace/src/apache-spark-on-k8s/spark-integration/e2e/spark/*.tgz -Dspark-dockerfiles-dir=/home/ramanathana/go-workspace/src/apache-spark-on-k8s/spark-integration/e2e/spark/dist/kubernetes/dockerfiles/ \
+          -Dspark-distro-tgz=$root/spark/*.tgz \
           -DextraScalaTestArgs="-Dspark.kubernetes.test.master=k8s://$cluster -Dspark.docker.test.driverImage=gcr.io/spark-k8s-test/spark-driver:latest -Dspark.docker.test.executorImage=gcr.io/spark-k8s-test:latest"
