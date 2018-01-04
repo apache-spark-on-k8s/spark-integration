5c411da7e3ab2a7160f9235cb1310366da7669ad
foxish <ramanathana@google.com>
2018-01-04 00:10:33 -0800

    Push changes to run-tests

--- e2e/run-tests.sh
+++ e2e/run-tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -ex
-<<<<<<< HEAD
 echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
 apt-get update && apt-get install -y curl wget git tar openjdk-8-jdk
 
@@ -15,13 +14,14 @@ git clone https://github.com/apache-spark-on-k8s/spark-integration
 cd spark && ./dev/make-distribution.sh --tgz -Phadoop-2.7 -Pkubernetes -DskipTests
 tag=(git rev-parse HEAD | cut -c -6)
 
-cd dist && ./sbin/build-push-docker-images.sh -r gcr.io/swarm-1358 -t $tag build && \
+cd dist && \
+  gcloud container builds submit --tag gcr.io/swarm-1358/spark-driver -f
+
+./sbin/build-push-docker-images.sh -r gcr.io/swarm-1358 -t $tag build && \
            gcloud docker -- push gcr.io/swarm-1358/spark-driver:$tag && \
            gcloud docker -- push gcr.io/swarm-1358/spark-executor:$tag && \
            gcloud docker -- push gcr.io/swarm-1358/spark-init:$tag
-
-cd ../../spark-integration/integration-test
-
+           
 cluster=kubectl cluster-info | head -n 1 | grep -oE "https?://\b([0-9]{1,3}\.){3}[0-9]{1,3}\b"
 mvn clean -Ddownload.plugin.skip=true integration-test \
           -Dspark-distro-tgz=$root/spark/*.tgz \
